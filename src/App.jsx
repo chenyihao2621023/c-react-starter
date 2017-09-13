7c794526eb6a06d2c7c9f9d1372780d0eeda54b0
cyh <372528184@qq.com>
2017-09-13 11:01:07 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default class App extends Component {
     render() {
         return (
             <div className="app">
-            hello World
+            hello World222
           </div>
         );
     }
